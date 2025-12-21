47bfbcba420d7a84191454243507e8420ccce6ae
unknown <cooley.blaine@gmail.com>
2025-12-21 01:03:23 -0500

    New files for functions and brought in shader import from other branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,13 @@
 #include "../include/glad.h"
 #include "../include/glfw3.h"
+#include "debug.h"
+#include "shaders.h"
 
 #include <iostream>
 #include <math.h>
 #include <stdio.h>
-#include <fstream>
-#include <string>
-#include <sstream>
 
-#define ASSERT(x) if (!(x)) __builtin_trap();
-
-#define GLCall(x) GLClearError();\
-   x;\
-   ASSERT(GLLogCall(#x, __FILE__, __LINE__))
-
-static void GLClearError()
-{
-    while (glGetError() != GL_NO_ERROR);
-}
-
-static bool GLLogCall(const char* function, const char* file, int line)
-{
-    while (GLenum error = glGetError())
-    {
-        std::cout<< "[OpenGL_Error] (" <<error << "): "<<function<<" " <<file<<":"<<line <<std::endl;
-        return false;
-    }
-    return true;
-}
+ 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -36,82 +16,7 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-struct ShaderProgramSource
-{
-    std::string VertexSource;
-    std::string FragmentSource;
-};
 
-static ShaderProgramSource ParseShader(const std::string& filepath) //cherno file reader https://www.youtube.com/watch?v=2pv0Fbo-7ms&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&index=8
-{
-    std::ifstream stream(filepath);
-
-    enum class ShaderType
-    {
-        NONE = -1, VERTEX =0, FRAGMENT = 1
-    };
-
-    std::string line;
-    std::stringstream ss[2];
-    ShaderType type = ShaderType::NONE;
-    while (getline(stream, line))
-    {
-        if(line.find("#shader") != std::string::npos)
-        {
-                if (line.find("vertex") != std::string::npos)
-                {
-                    type= ShaderType::VERTEX;
-                }
-                else if (line.find("fragment") != std::string::npos)
-                {
-                    type= ShaderType::FRAGMENT;
-                }
-        }
-        else
-        {
-            ss[(int)type] << line << '\n';
-        }
-    }
-    return { ss[0].str(), ss[1].str() };
-}
-static unsigned int CompileShader(unsigned int type, const std::string& source) //function for making and compiling vertex shaders
-{
-        unsigned int id =glCreateShader(type);
-        const char* src=source.c_str();
-        glShaderSource(id, 1, &src, nullptr);
-        glCompileShader(id);
-        
-        int result;
-        glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-        if (result == GL_FALSE)
-        {
-            int length;
-            glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-            char* message= (char*)alloca(length * sizeof(char));
-            glGetShaderInfoLog(id,length,&length,message);
-            std::cout<<"Failed to compile"<< (type==GL_VERTEX_SHADER ? "vertex" : "fragment") <<" shader!" <<std::endl;
-            std::cout<<message<<std::endl;
-            glDeleteShader(id);
-            return 0;
-        }
-        return id;
-}
-
-static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)//function for creating shader program
-{
-    unsigned int program =glCreateProgram();
-    unsigned int vs= CompileShader(GL_VERTEX_SHADER, vertexShader);
-    unsigned int fs= CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-    return program;
-}
 
 int main()
 {
